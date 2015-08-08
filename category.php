7e567a72aba0547443e04303d525ea4587eae25b
Gabriel Pinango <gabrielpinango1492@gmail.com>
2015-08-08 14:20:34 -0430

    html -> php

---
+++ category.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html>
+    <head>        
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+        <title>Areiro</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <meta name="description" content="Fullscreen Background Image Slideshow with CSS3 - A Css-only fullscreen background image slideshow" />
+        <meta name="keywords" content="areiro, cctv, camera" />
+        <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+        <script src='http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/js/bootstrap.min.js'></script>
+        <script src="js/script.js"></script>
+        <link rel="stylesheet" href="css/stylesheet.css" />
+        <link rel='stylesheet prefetch' href='http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/css/bootstrap.min.css'>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/stylesheet-carrousel.css">
+        <link rel="stylesheet" type="text/css" href="css/style3.css" />
+        <link rel="stylesheet" href="css/style.css">
+        <link rel='stylesheet prefetch' href='css/style2.css'>
+    </head>
+    <body>
+        <section id="main-wrapper">
+            <header>
+                <div id="top-nav" class="top-nav">
+                    <div class="wrapper">
+                        <form method="get" action="/search" id="search">
+                            <input name="q" id="q" type="search" size="40" placeholder="Search..." />
+                        </form>
+                        <nav class="nav">
+                            <ul>
+                                <a href="index.php"><li><i class="fa fa-home"></i> Home</li></a>
+                                <a href="#"><li><i class="fa fa-hand-o-right"></i> About us</li></a>
+                                <a href="#"><li><i class="fa fa-envelope-o"></i> Contact us</li></a>
+                                <a href="#"><li><i class="fa fa-fast-forward"></i> Videos</li></a>
+                                <a href="#"><li><i class="fa fa-video-camera"></i> LiveStream</li></a>
+                                <a href="#"><li><i class="fa fa-language"></i> Languague</li></a>               
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div id="main-nav">
+                    <div class="wrapper">
+                        <div class="logo"><span>LOGO</span></div>
+                        <nav class="nav">
+                            <ul>
+                                <a href="category.php"><li><i class="fa fa-bullhorn"></i> Alarmes</li></a>
+                                <a href="#"><li><i class="fa fa-clock-o"></i> Analogo</li></a>
+                                <a href="#"><li><i class="fa fa-hdd-o"></i> Ahd</li></a>
+                                <a href="#"><li><i class="fa fa-chevron-circle-right"></i> Cabos</li></a>
+                                <a href="#"><li><i class="fa fa-tags"></i> Accesorios</li></a>
+                                <a href="#"><li><i class="fa fa-eye"></i> Espião</li></a>
+                                <a href="#"><li><i class="fa fa-keyboard-o"></i> Informatica</li></a>
+                                <a href="#"><li><i class="fa fa-money"></i> Liquidação</li></a>      
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                
+                <div id="top-nav-small" >
+                    <div class="wrapper">
+                        <nav class="nav">
+                            <div class="logo"><span>LOGO</span></div>
+                            <div id="menu-bars"><a href="#"><i class="fa fa-bars"></i></a></div>
+                        </nav>
+                    </div>
+                </div>
+                <div id="main-nav-small">
+                    <div class="wrapper">
+                        <nav class="nav">
+                            <ul>
+                                <li>
+                                    <form method="get" action="/search" id="search">
+                                        <input name="q-small" id="q-small" type="search" size="40" placeholder="Search..." />
+                                    </form>
+                                </li>
+                                <a href="#"><li><i class="fa fa-bullhorn"></i> Alarmes</li></a>
+                                <a href="#"><li><i class="fa fa-clock-o"></i> Analogo</li></a>
+                                <a href="#"><li><i class="fa fa-hdd-o"></i> Ahd</li></a>
+                                <a href="#"><li><i class="fa fa-chevron-circle-right"></i> Cabos</li></a>
+                                <a href="#"><li><i class="fa fa-tags"></i> Accesorios</li></a>
+                                <a href="#"><li><i class="fa fa-eye"></i> Espião</li></a>
+                                <a href="#"><li><i class="fa fa-keyboard-o"></i> Informatica</li></a>
+                                <a href="#"><li><i class="fa fa-money"></i> Liquidação</li></a>      
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </header>
+            <section id="banner">
+                <div class="wrapper"><h2>TITLE</h2></div>
+            </section>
+            <div class="content" id="content-product">
+                <section>
+                    <div class="wrapper">
+                        <div id="pattern" class="pattern">
+                            <ul class="g">
+                                <li><a href="product.php"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a><a href="product.html">Product</a><p>Product description here djkndslkvndslkvnvnjnjvnkvklmmv  klmvmklcv klmcvkmcv kmcvklmcvkmlvc</p><p>Price: 999</p></li>
+                                <li><a href="product.html"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                            </ul>
+                            
+                            <ul class="g">
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                                <li><a href="#"><img src="http://bradfrost.github.com/this-is-responsive/patterns/images/fpo_square.png" alt="Product Name" /></a></li>
+                            </ul>
+                        </div>
+                        <input type='hidden' id='current_page' />
+                        <input type='hidden' id='show_per_page' />  
+                        <div id='page_navigation'></div>  
+                    </div>
+                </section>
+            </div>
+            <footer>
+                <div class="wrapper">
+                    <ul>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Contact us</a></li>
+                    </ul>
+                </div>
+            </footer>
+        </section>
+    </body>
+</html>
---
+++ product.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html>
+    <head>        
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+        <title>Areiro</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <meta name="description" content="Fullscreen Background Image Slideshow with CSS3 - A Css-only fullscreen background image slideshow" />
+        <meta name="keywords" content="areiro, cctv, camera" />
+        <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+        <script src='http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/js/bootstrap.min.js'></script>
+        <script src="js/script.js"></script>
+        <link rel="stylesheet" href="css/stylesheet.css" />
+        <link rel='stylesheet prefetch' href='http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/css/bootstrap.min.css'>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/stylesheet-carrousel.css">
+        <link rel="stylesheet" type="text/css" href="css/style3.css" />
+        <link rel="stylesheet" href="css/style.css">
+        <link rel='stylesheet prefetch' href='css/style2.css'>
+    </head>
+    <body>
+        <section id="main-wrapper">
+            <header>
+                <div id="top-nav" class="top-nav">
+                    <div class="wrapper">
+                        <form method="get" action="/search" id="search">
+                            <input name="q" id="q" type="search" size="40" placeholder="Search..." />
+                        </form>
+                        <nav class="nav">
+                            <ul>
+                                <a href="index.php"><li><i class="fa fa-home"></i> Home</li></a>
+                                <a href="#"><li><i class="fa fa-hand-o-right"></i> About us</li></a>
+                                <a href="#"><li><i class="fa fa-envelope-o"></i> Contact us</li></a>
+                                <a href="#"><li><i class="fa fa-fast-forward"></i> Videos</li></a>
+                                <a href="#"><li><i class="fa fa-video-camera"></i> LiveStream</li></a>
+                                <a href="#"><li><i class="fa fa-language"></i> Languague</li></a>               
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div id="main-nav">
+                    <div class="wrapper">
+                        <div class="logo"><span>LOGO</span></div>
+                        <nav class="nav">
+                            <ul>
+                                <a href="category.php"><li><i class="fa fa-bullhorn"></i> Alarmes</li></a>
+                                <a href="#"><li><i class="fa fa-clock-o"></i> Analogo</li></a>
+                                <a href="#"><li><i class="fa fa-hdd-o"></i> Ahd</li></a>
+                                <a href="#"><li><i class="fa fa-chevron-circle-right"></i> Cabos</li></a>
+                                <a href="#"><li><i class="fa fa-tags"></i> Accesorios</li></a>
+                                <a href="#"><li><i class="fa fa-eye"></i> Espião</li></a>
+                                <a href="#"><li><i class="fa fa-keyboard-o"></i> Informatica</li></a>
+                                <a href="#"><li><i class="fa fa-money"></i> Liquidação</li></a>      
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                
+                <div id="top-nav-small" >
+                    <div class="wrapper">
+                        <nav class="nav">
+                            <div class="logo"><span>LOGO</span></div>
+                            <div id="menu-bars"><a href="#"><i class="fa fa-bars"></i></a></div>
+                        </nav>
+                    </div>
+                </div>
+                <div id="main-nav-small">
+                    <div class="wrapper">
+                        <nav class="nav">
+                            <ul>
+                                <li>
+                                    <form method="get" action="/search" id="search">
+                                        <input name="q-small" id="q-small" type="search" size="40" placeholder="Search..." />
+                                    </form>
+                                </li>
+                                <a href="#"><li><i class="fa fa-bullhorn"></i> Alarmes</li></a>
+                                <a href="#"><li><i class="fa fa-clock-o"></i> Analogo</li></a>
+                                <a href="#"><li><i class="fa fa-hdd-o"></i> Ahd</li></a>
+                                <a href="#"><li><i class="fa fa-chevron-circle-right"></i> Cabos</li></a>
+                                <a href="#"><li><i class="fa fa-tags"></i> Accesorios</li></a>
+                                <a href="#"><li><i class="fa fa-eye"></i> Espião</li></a>
+                                <a href="#"><li><i class="fa fa-keyboard-o"></i> Informatica</li></a>
+                                <a href="#"><li><i class="fa fa-money"></i> Liquidação</li></a>      
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </header>
+            <div class="content" id="section-product">
+                <div id="title" class="wrapper">
+                    <label><h1>TITLE</h1></label>
+                </div>
+                <div class="wrapper-product">
+                    <div id="wrapper-item">
+                    <div id="img"><img src="http://placehold.it/360x240" alt=""></div>
+                    <div id="info">
+                        <div id="price"><label><h2>Price: 999$usd</h2></label></div>
+                        <div id="item-description">
+                            ITEM DESCRIPTION
+                            <div id="item-line"></div>
+                            <div id="item-info">
+                                <h5>Te est error verear ponderum, no vix habeo noster. Ex nostro petentium interpretaris vix, ut qui quodsi placerat constituto.</h5>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                    
+                    <!-- END OF SLIDER -->
+                    
+                    <div id="description">
+                        Te est error verear ponderum, no vix habeo noster. Ex nostro petentium interpretaris vix, ut qui quodsi placerat constituto. Ut commodo eripuit intellegebat vix, ei vel dicta dolore audire. Et appareat sadipscing efficiantur his, veniam audiam adipiscing ea nec, ei habemus convenire principes ius. Vis ei atomorum tractatos. His inani falli ad. Ex sale liber mea.
+
+Per nostrum constituto at. Eu has erant impedit referrentur, mea ad aeterno nostrud facilis. No abhorreant ullamcorper eos. Intellegam deterruisset ea sit, dicit eloquentiam duo et, qui dicat dolore ea.
+
+At vel omnis possim complectitur, ex prompta assueverit vix, ceteros honestatis his ei. Vix affert doctus vituperatoribus cu. Partem philosophia est an, eu pro omnes corpora. Has nulla iudico consequuntur ei, mucius temporibus disputando pri ne. Sed nullam sensibus mnesarchum ex.
+
+Malorum voluptaria eu mel. Eu quaeque indoctum suavitate per, sed ea ullum instructior. Brute aliquip atomorum ut mei, est no alii persius. Per ad cibo habemus honestatis, assum malorum cum cu. Laoreet meliore ne ius, ius augue ridens persius in.
+
+Tibique consetetur cu sea, id iuvaret maiorum mel. Tritani invidunt intellegebat pro at, salutandi hendrerit vituperatoribus an sea, id quaeque delicatissimi qui. Epicuri definitionem vis at, eos ut vide illum, usu iracundia pertinacia te. Mea ei sonet ponderum, ei ius veniam patrioque, an sit nulla dolores similique.
+
+Ei eam choro oblique definitionem. Ea est everti quaeque detracto. Facete repudiare ei usu. Ea mei melius hendrerit, dolore diceret et nec, alia tempor ad est.
+
+At facer corrumpit est. At ius justo veniam, eam falli affert definitionem eu, at vim oblique vivendo temporibus. Ea vel mundi malorum feugiat, ei docendi oporteat disputationi est. Id vitae malorum duo. Ei mei case mundi tibique, quod delectus mei et.
+
+Id cum pertinax percipitur, recteque accusamus mea ex. Novum quidam vel ei, usu cu audire iracundia. Ea duo epicuri noluisse, enim posse patrioque mei at. Et amet nusquam mea, eam et wisi iuvaret.
+
+Velit vituperatoribus vim in, in tamquam debitis sea. Ne fabulas vivendo sit. Graeci detraxit eum ei, in quo essent reformidans. Tation persius an quo, id mel natum illum doctus. Cu eam ullum scripserit.
+
+Prompta corpora voluptaria at sed, quo eu vidisse electram imperdiet. Viris offendit euripidis eu sed, nam sonet invenire disputationi cu. Et pro nobis ceteros convenire, et mea stet petentium, nonumes conclusionemque sit ad. Agam delectus scriptorem vim at, ut utinam graeco ornatus vis, ut mundi legere qui. Id ignota probatus electram qui, ea eos soleat verear oportere, iriure option eu vix. Quidam verear bonorum per an, at per probo nullam scriptorem.
+                    </div>
+                </div>
+            </div>
+            <footer>
+            <div class="wrapper">
+                <ul>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Contact us</a></li>
+                </ul>
+            </div>
+        </footer>
+        </section>
+    </body>
+</html>
